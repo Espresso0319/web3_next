bc3c7639e6b6f596a939e2bd029d31a99d1cb627
erick <espresso0319@gmail.com>
2025-03-12 15:02:38 +0800

    enable globals css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import React from 'react'
 import { WagmiProviderWrapper } from './providers/WagmiProvider'
+import "./globals.css";
 
 export const metadata = {
   title: 'Wagmi & Next.js App Router Demo',
